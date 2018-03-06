42e97154005f18cc1d638762f48fbe0c94bc2b32
Aaron Fay <aaron.j.fay@gmail.com>
2018-03-06 14:08:40 -0700

    add circle config for standalone test

---
+++ test/standalone-app/index.js
@@ -0,0 +1,28 @@
+/**
+ * Just a simple app to test the standalone Dockerfile to ensure that the
+ * environment variables and param passing are working.
+ */
+var http = require('http'); 
+
+http.createServer(function (req, res) { 
+  res.writeHead(200, {'Content-Type': 'text/html'}); 
+  res.end(`
+<html>
+  <body>
+    <h2>Standalone image test</h2>
+    <p>
+      This is a standalone application that is networked to our
+      <code>ghostinspector/test-runner-standalone</code> container for the
+      purposes of testing. The SHA value below was passed to both containers
+      for verification under test, whereas this container will display
+      the SHA value on the page, and the other container will send the value
+      to the Ghost Inspector test suite where they will be compared.
+    </p>
+    <ul>
+      <li id="sha">${process.argv[2]}</li>
+    </ul>
+  </body>
+</html>
+  `); 
+}).listen(8000);
+console.log('Server running on port 8000.');
