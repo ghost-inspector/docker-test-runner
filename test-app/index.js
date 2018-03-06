b105471b92f0374ec2c2595b6e4207cd8a915801
Aaron Fay <aaron.j.fay@gmail.com>
2018-03-06 09:01:36 -0700

    add circle config

---
+++ test-app/index.js
@@ -0,0 +1,20 @@
+/**
+ * Just a simple app to test the base-image Dockerfile to ensure that the
+ * environment variables and param passing are working.
+ */
+var http = require('http'); 
+
+http.createServer(function (req, res) { 
+  res.writeHead(200, {'Content-Type': 'text/html'}); 
+  res.end(`
+<html>
+  <body>
+    <ul>
+      <li id="envVar">${process.env.MY_ENV_VAR}</li>
+      <li id="paramVar">${process.argv.pop()}</li>
+    </ul>
+  </body>
+</html>
+  `); 
+}).listen(8000);
+console.log('Server running on port 8000.');
